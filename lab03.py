f23596d5df1ee9233979cbcbf627cc2b21f2dfaf
MichéLozano <elmiche@latinooutdoors.org>
2019-10-03 19:30:49 -0700

    bug fix

--- lab03.py
+++ lab03.py
@@ -7,13 +7,13 @@ user_score = input("Find out your letter grade, enter your score:")
 user_grade = int(user_score)
 
 #this is the code that converts numeric ranges to symbolic letters
-if user_grade >= 90 and <= 100
+if user_grade >= 90 and <= 100:
     print("A")
-if user_grade >= 80 and <= 89
+if user_grade >= 80 and <= 89:
     print("B")
-if user_grade >= 70 and <= 79
+if user_grade >= 70 and <= 79:
     print("C")
-if user_grade >= 60 and <= 69
+if user_grade >= 60 and <= 69:
     print("D")
-if user_grade >= 50 and <= 59
+if user_grade >= 50 and <= 59:
     print("F")
