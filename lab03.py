6be12ff033c6fb9b723fe3e61e08287ad928d96d
MichéLozano <elmiche@latinooutdoors.org>
2019-10-03 19:41:45 -0700

    version 1 completed

--- lab03.py
+++ lab03.py
@@ -1,19 +1,22 @@
 #lab03.py
 #Grading
-# TO DO: Practice Branching
 
-
+#version 1
 user_score = input("Find out your letter grade, enter your score:")
 user_grade = int(user_score)
 
 #this is the code that converts numeric ranges to symbolic letters
-if user_grade >= 90 and <= 100:
+if user_grade >= 90 and user_grade <=100:
     print("A")
-if user_grade >= 80 and <= 89:
+elif user_grade >= 80 and user_grade <=89:
     print("B")
-if user_grade >= 70 and <= 79:
+elif user_grade >= 70 and user_grade <=79:
     print("C")
-if user_grade >= 60 and <= 69:
+elif user_grade >= 60 and user_grade <=69:
     print("D")
-if user_grade >= 50 and <= 59:
+elif user_grade >= 0 and user_grade <=59:
     print("F")
+else: done
+
+
+#version
